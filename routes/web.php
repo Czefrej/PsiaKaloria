758de5bc3f23a0a8161edcbebab047086e1e5ea7
Czefrej <tavrosteam@gmail.com>
2021-09-08 21:05:23 +0200

    git

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('example');
 });
 Route::get('/example', function () {
     return view('example');
