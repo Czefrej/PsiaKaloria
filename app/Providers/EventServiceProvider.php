be803b6465c63c769f82669b9be4fbad01a0d03b
Shift <shift@laravelshift.com>
2023-02-08 19:17:44 +0000

    Shift cleanup

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -60,4 +60,3 @@ class EventServiceProvider extends ServiceProvider
         return false;
     }
 }
-
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\BaselinkerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
